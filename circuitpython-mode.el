9b541bfc114d4b88ee2154e04af32dd06a81ddd4
Lee Nelson <lnelson@nelnet.org>
2020-07-17 21:22:07 -0700

    Fix org-mode headers in README

--- circuitpython-mode.el
+++ circuitpython-mode.el
@@ -6,64 +6,83 @@
 ;; URL: https://github.com/nelnet-team/circuitpython-mode
 
 ;;; Commentary:
-;; ## This is in early development.  Only working code will be committed to
-;; MELPA Stable, but the current functionality is limited
+;; 1 circuitpython-mode.el
+;; ═══════════════════════
 
-;; ## Description This package provides a minor mode called
-;; `circuitpython-mode'.  This mode is intended to help with the workflow
-;; in circuitpython development. It is invoked by typing:
+;; 1.1 This is in early development.
+;; ─────────────────────────────────
 
-;; ┌────
-;; │ M-x circuitpython-mode
-;; └────
+;;   Only working code will be committed to MELPA Stable, but the current
+;;   functionality is limited
 
-;; ## Usage
 
-;; ### Compile (copy file to board)
+;; 1.2 Description
+;; ───────────────
 
-;; A function, `circuitpython-compile-copy', is added that is invoked each
-;; time the buffer is saved (after-save-hook). When invoked, the new
-;; command will update the compile-command to be a shell command that will
-;; copy the current file to a different directory (ie the board).  The
-;; destination directory needs to be defined as a *file-local* or
-;; *dir-local* variable.  After each file save (**[C-x C-s]**, **[C-x w]**,
-;; 					       etc), the default compile-command will be something like:
+;;   This package provides a minor mode called `circuitpython-mode'.  This
+;;   mode is intended to help with the workflow in circuitpython
+;;   development. It is invoked by typing:
 
-;; ┌────
-;; │ cp filename.py /some/path/some/where/
-;; └────
+;;   ┌────
+;;   │ M-x circuitpython-mode
+;;   └────
 
-;; This does not modify the behavior of `compile' itself, it only changes
-;; the command that will presented as the default compile command when
-;; invoking `compile' (usually **[C-c c]** )
 
-;; ### Compile .mpy
+;; 1.3 Usage
+;; ─────────
 
-;; Additionally, a new command, `circuitpython-mpy-compile', is added and
-;; bound to **[C-c m]**.  If the variable `mpy-compiler' is defined (ie as
-;; 								     file-local or dir-local), then that command will be used.  Otherwise the
-;; command "mpy-cross" is used.  One potentional reason to specify the
-;; mpy-compiler is if it is not in $PATH. This will define the
-;; compile-command to be something like:
+;; 1.3.1 Compile (copy file to board)
+;; ╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌
 
-;; ┌────
-;; │ mpy-cross filename.py
-;; └────
+;;   A function, `circuitpython-compile-copy', is added that is invoked
+;;   each time the buffer is saved (after-save-hook). When invoked, the new
+;;   command will update the compile-command to be a shell command that
+;;   will copy the current file to a different directory (ie the board).
+;;   The destination directory needs to be defined as a *file-local* or
+;;   *dir-local* variable.  After each file save (**[C-x C-s]**, **[C-x
+;;   w]**, etc), the default compile-command will be something like:
 
-;; There is also a command, `circuitpython-set-mpy-compiler', bound to
-;; **[C-c n]** that allows the user to override the process described above
-;; and provide an alternative command.
+;;   ┌────
+;;   │ cp filename.py /some/path/some/where/
+;;   └────
 
-;; After the mpy compile command is defined, `compile' is called, after
-;; which `circuitpython-compile-copy' (see above) is called. This means
-;; that after compiling an .mpy, the compile command will revert back to
-;; being the command to copy the file to the board.
+;;   This does not modify the behavior of `compile' itself, it only changes
+;;   the command that will presented as the default compile command when
+;;   invoking `compile' (usually **[C-c c]** )
 
-;; ## Example directory local variables `.dir-locals.el'
 
-;; ┌────
-;; │ ((nil . ((circuitpython-copy-path . "/mnt/chromeos/removable/CIRCPY/"))))
-;; └────
+;; 1.3.2 Compile .mpy
+;; ╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌
+
+;;   Additionally, a new command, `circuitpython-mpy-compile', is added and
+;;   bound to **[C-c m]**.  If the variable `mpy-compiler' is defined (ie
+;;   as file-local or dir-local), then that command will be used.
+;;   Otherwise the command "mpy-cross" is used.  One potentional reason to
+;;   specify the mpy-compiler is if it is not in $PATH. This will define
+;;   the compile-command to be something like:
+
+;;   ┌────
+;;   │ mpy-cross filename.py
+;;   └────
+
+;;   There is also a command, `circuitpython-set-mpy-compiler', bound to
+;;   **[C-c n]** that allows the user to override the process described
+;;   above and provide an alternative command.
+
+;;   After the mpy compile command is defined, `compile' is called, after
+;;   which `circuitpython-compile-copy' (see above) is called. This means
+;;   that after compiling an .mpy, the compile command will revert back to
+;;   being the command to copy the file to the board.
+
+
+;; 1.4 Example directory local variables
+;; ─────────────────────────────────────
+
+;;   `.dir-locals.el'
+
+;;   ┌────
+;;   │ ((nil . ((circuitpython-copy-path . "/mnt/chromeos/removable/CIRCPY/"))))
+;;   └────
 
 (setq circuitpython-current-mpy-compiler "mpy-cross")
 
